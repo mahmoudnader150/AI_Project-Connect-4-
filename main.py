cee2b127d1f8c59aebeb3d6e66c18d22a88753aa
mahmoudnader150 <91666764+mahmoudnader150@users.noreply.github.com>
2023-05-13 18:23:02 +0300

    Update logic of code to check winning action

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ COLUMN_COUNT = 7
 
 
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 
@@ -16,7 +16,7 @@ def drop_piece(board, row, col, piece):
 
 # valid to put the piece
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -30,10 +30,37 @@ def print_board(board):
     print(np.flip(board, 0))
 
 
+def winning_move(board, piece):
+    # check horizontal locations
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # check horizontal locations
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+
+    # positive diagonls
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+
+    # negative diagonals
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
+
+
 board = create_board()
 print_board(board)
 game_over = False
 turn = 0
+
 
 # while game not ende
 while not game_over:
@@ -45,6 +72,9 @@ while not game_over:
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
 
+            if winning_move(board, 1):
+                print("* CONGRATULATIONS * Player 1 wins!!")
+                game_over = True
     # Player 2 input
     else:
         col = int(input("Player 2 Choose your column(0-6):"))
@@ -52,6 +82,9 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+            if winning_move(board, 2):
+                print("* CONGRATULATIONS * Player 2 wins!!")
+                game_over = True
 
     print_board(board)
     # exchang turns
