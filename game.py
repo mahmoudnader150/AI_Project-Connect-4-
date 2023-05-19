eaf3a6a2332ffb1304e1c0d73334562d3cdeb41a
nadeenmedhat <nadeenmedhat12125@gmail.com>
2023-05-19 14:57:00 +0300

    updated game file

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import random
 import numpy as np
 import main as mn
 import math
+import tkinter as tk
 
 # GAME LINK
 # http://kevinshannon.com/connect4/
@@ -17,6 +18,8 @@ BOARD = np.zeros((ROW_COUNT, COLUMN_COUNT))
 def main():
     board = Board()
 
+    depth = choose_depth()
+
     time.sleep(2)
     game_end = False
     while not game_end:
@@ -25,28 +28,30 @@ def main():
         # FOR DEBUG PURPOSES
         board.print_grid(game_board)
 
-        # YOUR CODE GOES HERE
-
         for r in range(ROW_COUNT):
             for c in range(COLUMN_COUNT):
-                if(game_board[r][c] == 0):
+                if game_board[r][c] == 0:
                     BOARD[r, c] = 0
-                if(game_board[r][c] == 1):
+                if game_board[r][c] == 1:
                     BOARD[r, c] = 1
-                if(game_board[r][c] == 2):
+                if game_board[r][c] == 2:
                     BOARD[r, c] = 2
 
         print(BOARD)
 
-        # Insert here the action you want to perform based on the output of the algorithm
-        # You can use the following function to select a column
-        # col, minimax_score = mn.minimax(BOARD, 3, -math.inf, math.inf, True)
-        col, minimax_score = mn.minimax(BOARD, 5, -math.inf, math.inf, True)
+        col, minimax_score = mn.minimax(
+            BOARD, depth, -math.inf, math.inf, True)
 
+        # if mn.is_valid_location(BOARD, col):
+        #     row = mn.get_next_open_row(BOARD, col)
+        #     mn.drop_piece(BOARD, row, col, 2)
+        #     if mn.winning_move(BOARD, 2):
+        #         game_over = True
         if mn.is_valid_location(BOARD, col):
-            # delay time
+            # pygame.time.wait(500)
             row = mn.get_next_open_row(BOARD, col)
             mn.drop_piece(BOARD, row, col, 2)
+
             if mn.winning_move(BOARD, 2):
                 game_over = True
 
@@ -56,7 +61,6 @@ def main():
         time.sleep(2)
         if mn.winning_move(BOARD, 1) or mn.winning_move(BOARD, 2):
             break
-        game_end = mn.is_terminal_nade(BOARD)
 
 
 if __name__ == "__main__":
